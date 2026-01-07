3bf3ec3e2e4d686c86e8d09bea10b16e65ca5dee
Jordan Epstein <jordepstein@gmail.com>
2026-01-06 19:22:11 -0600

    DH-53 Make images work in GitHub pages docs
    Use script to copy readme.md to docs page, as well as images.

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-cp README.md docs/readme.md
+
+cat > docs/index.md << 'EOF'
+---
+layout: page
+title: Documentation
+permalink: /docs/
+---
+
+EOF
+
+sed 's|!\[\(.*\)\](\(read_path\.png\))|![\1]({{ site.baseurl }}/\2)|g; s|!\[\(.*\)\](\(write_path\.png\))|![\1]({{ site.baseurl }}/\2)|g' readme.md >> docs/index.md
+
 BUNDLE_PATH=vendor/bundle bundle exec jekyll serve --baseurl ""
